ee501228a9b58c141787e66a5f030690668eca5c
Steve Kellock <steve@kellock.ca>
2017-03-04 11:46:39 -0500

    Fixes deprecations.

--- plugin.js
+++ plugin.js
@@ -11,7 +11,7 @@ const add = async function (context) {
   await ignite.addModule(NPM_MODULE_NAME)
 
   // copy the example file (if examples are turned on)
-  await ignite.addComponentExample(EXAMPLE_FILE, { title: 'Animatable' })
+  await ignite.addPluginComponentExample(EXAMPLE_FILE, { title: 'Animatable' })
 }
 
 /**
@@ -24,7 +24,7 @@ const remove = async function (context) {
   await ignite.removeModule(NPM_MODULE_NAME)
 
   // remove the component example
-  await ignite.removeComponentExample(EXAMPLE_FILE)
+  await ignite.removePluginComponentExample(EXAMPLE_FILE)
 }
 
 /**
